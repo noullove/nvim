a16d7279ae9e1593ceab412fa68ec5e15e43e3bc
noullove <noullove@gmail.com>
2025-01-18 22:24:01 +0900

    first commit

---
+++ lua/mappings.lua
@@ -0,0 +1,57 @@
+require "nvchad.mappings"
+
+-- add yours here
+local map = vim.keymap.set
+local autocmd = vim.api.nvim_create_autocmd
+
+-- restore cursor position
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
+-- dynamic terminal padding
+autocmd("VimEnter", {
+  command = ":silent !kitty @ set-spacing padding=0 margin=0",
+})
+
+autocmd("VimLeavePre", {
+  command = ":silent !kitty @ set-spacing padding=20 margin=10",
+})
+
+-- keymap
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>lm", function()
+   require("telescope.builtin").lsp_document_symbols({symbols={"method","function"}})
+end, { desc = "LSP document symbols" } )
+map("n", "<leader>cd", require("telescope").extensions.zoxide.list, { desc = "Telescope zoxide list" })
+
+-- volt menu
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open("default")
+end, {})
+
+-- volt menu
+-- mouse users + nvimtree users!
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require('menu.utils').delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, {})
---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+o.fileencodings = 'utf-8,euc-kr,cp949'
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
